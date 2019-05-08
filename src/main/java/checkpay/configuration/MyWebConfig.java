c61ca6d28e0d8564c4ca72766d41ec2c16532bab
jethronap <efthioa@gmail.com>
2019-05-08 10:44:30 +0300

    removed used imports

--- src/main/java/checkpay/configuration/MyWebConfig.java
+++ src/main/java/checkpay/configuration/MyWebConfig.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.ViewResolver;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
