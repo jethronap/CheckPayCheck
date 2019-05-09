0fff2c5a7d4ba2da408727e737b5db9c4be8c594
jethronap <efthioa@gmail.com>
2019-05-09 18:51:50 +0300

    security tryout 3

--- src/main/java/checkpay/models/Role.java
+++ src/main/java/checkpay/models/Role.java
@@ -1,10 +1,12 @@
 package checkpay.models;
 
+import java.util.Set;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
 /**
@@ -23,6 +25,9 @@ public class Role {
     @Column(name = "name")
     private String name;
 
+    @ManyToMany(mappedBy = "roles")
+    private Set<User> users;
+    
     public Role() {
     }
 
@@ -49,5 +54,13 @@ public class Role {
     @Override
     public String toString() {
         return "Role{" + "id=" + id + ", name='" + name + '\'' + '}';
+    }
+
+    public Set<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(Set<User> users) {
+        this.users = users;
     }
 }
