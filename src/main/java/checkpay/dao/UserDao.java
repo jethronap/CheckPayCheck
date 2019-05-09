e0439f1b26c1f4ef0613170613dfd0247d796d30
jethronap <efthioa@gmail.com>
2019-05-09 12:41:25 +0300

    updated user dao & daoImpl

--- src/main/java/checkpay/dao/UserDao.java
+++ src/main/java/checkpay/dao/UserDao.java
@@ -20,4 +20,7 @@ public interface UserDao {
     List<User> findAllUsers();
 
     User findUserById(int ssn);
+
+    User findUserByEmail(String email);
+
 }
--- src/main/java/checkpay/dao/UserDaoImpl.java
+++ src/main/java/checkpay/dao/UserDaoImpl.java
@@ -12,48 +12,58 @@ import org.springframework.stereotype.Repository;
  *
  * @author jnap
  */
-
 @Repository("userDao")
-public class UserDaoImpl extends AbstractDao<Integer, User> implements UserDao{
+public class UserDaoImpl extends AbstractDao<Integer, User> implements UserDao {
+
     @Override
-	public User findById(int id) {
-		return getByKey(id);
-	}
+    public User findById(int id) {
+        return getByKey(id);
+    }
 
-        @Override
-	public void saveUser(User user) {
-		persist(user);
-	}
-	
-        @Override
-	public void saveOrUpdate(User user){
-		super.saveOrUpdate(user);
-	}
-	
-        @Override
-	public void deleteUserById(int id) {
-		Query query = getSession().createNativeQuery("delete from user where id = :id");
-		query.setParameter("id", id);
-		query.executeUpdate();
-	}
+    @Override
+    public void saveUser(User user) {
+        persist(user);
+    }
 
-	@SuppressWarnings("unchecked")
-        @Override
-	public List<User> findAllUsers() {
-		CriteriaQuery<User> criteria = createEntityCriteria();
-                Root<User> root = criteria.from(User.class);
-                criteria.select(root);
-                Query query = getSession().createQuery(criteria);
-		return (List<User>) query.getResultList();
-	}
+    @Override
+    public void saveOrUpdate(User user) {
+        super.saveOrUpdate(user);
+    }
 
-        @Override
-	public User findUserById(int id) {
-                CriteriaBuilder builder = getSession().getCriteriaBuilder();
-		CriteriaQuery<User> criteria = builder.createQuery(User.class);
-		Root<User> root = criteria.from(User.class);
-                criteria.select(root).where(builder.equal(root.get("id"), id));
-                Query<User> q=getSession().createQuery(criteria);
-		return (User) q.getSingleResult();
-	}
+    @Override
+    public void deleteUserById(int id) {
+        Query query = getSession().createNativeQuery("delete from user where id = :id");
+        query.setParameter("id", id);
+        query.executeUpdate();
+    }
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public List<User> findAllUsers() {
+        CriteriaQuery<User> criteria = createEntityCriteria();
+        Root<User> root = criteria.from(User.class);
+        criteria.select(root);
+        Query query = getSession().createQuery(criteria);
+        return (List<User>) query.getResultList();
+    }
+
+    @Override
+    public User findUserById(int id) {
+        CriteriaBuilder builder = getSession().getCriteriaBuilder();
+        CriteriaQuery<User> criteria = builder.createQuery(User.class);
+        Root<User> root = criteria.from(User.class);
+        criteria.select(root).where(builder.equal(root.get("id"), id));
+        Query<User> q = getSession().createQuery(criteria);
+        return (User) q.getSingleResult();
+    }
+
+    @Override
+    public User findUserByEmail(String email) {
+        CriteriaBuilder builder = getSession().getCriteriaBuilder();
+        CriteriaQuery<User> criteria = builder.createQuery(User.class);
+        Root<User> root = criteria.from(User.class);
+        criteria.select(root).where(builder.equal(root.get("email"), email));
+        Query<User> q = getSession().createQuery(criteria);
+        return (User) q.getSingleResult();
+    }
 }
