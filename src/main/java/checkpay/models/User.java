033e6c04b5c102607a423084e616291dccd844b8
jethronap <efthioa@gmail.com>
2019-05-11 00:55:24 +0300

    updated user model

--- src/main/java/checkpay/models/User.java
+++ src/main/java/checkpay/models/User.java
@@ -1,30 +1,53 @@
 package checkpay.models;
 
+import java.util.HashSet;
+import java.util.Objects;
 import javax.persistence.*;
 import java.util.Set;
+import org.hibernate.validator.constraints.NotEmpty;
 
 @Entity
 @Table(name = "user")
 public class User {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Integer id;
 
+    @NotEmpty
+    @Column(name = "username", unique = true, nullable = false)
     private String username;
 
+    @NotEmpty
+    @Column(name = "password", nullable = false)
     private String password;
 
-    @Transient
-    private String passwordConfirm;
+    @NotEmpty
+    @Column(name = "fname", nullable = false)
+    private String fname;
 
-    @ManyToMany
-    private Set<Role> roles;
+    @NotEmpty
+    @Column(name = "lname", nullable = false)
+    private String lname;
 
-    public Long getId() {
+    @NotEmpty
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @NotEmpty
+    @ManyToMany(fetch = FetchType.LAZY)
+    @JoinTable(name = "user_role",
+            joinColumns = {
+                @JoinColumn(name = "user_id")},
+            inverseJoinColumns = {
+                @JoinColumn(name = "role_id")})
+    private Set<Role> roles = new HashSet<Role>();
+
+    public Integer getId() {
         return id;
     }
 
-    public void setId(Long id) {
+    public void setId(Integer id) {
         this.id = id;
     }
 
@@ -44,12 +67,28 @@ public class User {
         this.password = password;
     }
 
-    public String getPasswordConfirm() {
-        return passwordConfirm;
+    public String getFname() {
+        return fname;
     }
 
-    public void setPasswordConfirm(String passwordConfirm) {
-        this.passwordConfirm = passwordConfirm;
+    public void setFname(String fname) {
+        this.fname = fname;
+    }
+
+    public String getLname() {
+        return lname;
+    }
+
+    public void setLname(String lname) {
+        this.lname = lname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     public Set<Role> getRoles() {
@@ -59,4 +98,41 @@ public class User {
     public void setRoles(Set<Role> roles) {
         this.roles = roles;
     }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 67 * hash + Objects.hashCode(this.id);
+        hash = 67 * hash + Objects.hashCode(this.username);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final User other = (User) obj;
+        if (!Objects.equals(this.username, other.username)) {
+            return false;
+        }
+        if (!Objects.equals(this.password, other.password)) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" + "id=" + id + ", username=" + username
+                + ", fname=" + fname + ", lname=" + lname + ", email=" + email
+                + ", roles=" + roles + '}';
+    }
+
 }
